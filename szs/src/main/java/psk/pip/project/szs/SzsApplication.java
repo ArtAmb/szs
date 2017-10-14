4161b63304f704c965335baa3935564e4cff3c48
Użykownik <Uykownik@192.168.1.20>
2017-10-14 17:17:57 +0200

    first commit

---
+++ szs/src/main/java/psk/pip/project/szs/SzsApplication.java
@@ -0,0 +1,12 @@
+package psk.pip.project.szs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzsApplication.class, args);
+	}
+}
