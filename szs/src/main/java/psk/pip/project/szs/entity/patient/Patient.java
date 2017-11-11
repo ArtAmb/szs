62d1da57c08d07e55cf1a39c22127d76c925531b
Daniel <Daniel@192.168.0.11>
2017-11-11 17:04:06 +0100

    patient entity

---
+++ szs/src/main/java/psk/pip/project/szs/entity/patient/Patient.java
@@ -0,0 +1,21 @@
+package psk.pip.project.szs.entity.patient;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+
+public class Patient {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String Imie;
+	private String Nazwisko;	
+}
+
