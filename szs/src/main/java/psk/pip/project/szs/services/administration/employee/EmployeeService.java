34f9946e36bbb3c3a406fd7b4e85806c8fdf61f1
ArtAmb <artur.ambrolewicz@gmail.com>
2018-01-27 15:26:08 +0100

    searcher dla pielegniarek

--- szs/src/main/java/psk/pip/project/szs/services/administration/employee/EmployeeService.java
+++ szs/src/main/java/psk/pip/project/szs/services/administration/employee/EmployeeService.java
@@ -23,12 +23,22 @@ public class EmployeeService {
 	}
 
 	public Collection<SearcherResponse> findDoctorsByQueryStr(String query) {
-		return employeeRepository.findByQuery(query).stream().map(e -> SearcherMapper.map(e))
+		return employeeRepository.findDoctorsByQuery(query).stream().map(e -> SearcherMapper.map(e))
 				.collect(Collectors.toList());
 	}
 
 	public Collection<SearcherResponse> findDoctorsByQueryStrWithFilerOnInTeam(String query, boolean inTeam) {
-		return employeeRepository.findByQuery(query).stream().filter(d -> d.isInTeam() == inTeam)
+		return employeeRepository.findDoctorsByQuery(query).stream().filter(d -> d.isInTeam() == inTeam)
+				.map(e -> SearcherMapper.map(e)).collect(Collectors.toList());
+	}
+
+	public Collection<SearcherResponse> findNursesByQueryStr(String query) {
+		return employeeRepository.findNursesByQuery(query).stream().map(e -> SearcherMapper.map(e))
+				.collect(Collectors.toList());
+	}
+
+	public Collection<SearcherResponse> findNursesByQueryStrWithFilerOnInTeam(String query, boolean inTeam) {
+		return employeeRepository.findNursesByQuery(query).stream().filter(d -> d.isInTeam() == inTeam)
 				.map(e -> SearcherMapper.map(e)).collect(Collectors.toList());
 	}
 
