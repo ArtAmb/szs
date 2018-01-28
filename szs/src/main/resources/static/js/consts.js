d41ee0848624a01853ba479894698f6764babd2d
Karolina <karolinabatkowska@op.pl>
2018-01-28 22:50:14 +0100

    Badanie html (niepełne)

--- szs/src/main/resources/static/js/consts.js
+++ szs/src/main/resources/static/js/consts.js
@@ -3,6 +3,7 @@ var consts = {
     DTO_VALUE_ATTR: "dtovalue",
     URLS: {
         getMeasurementUnits: '/config/measurement-type/all',
-        getDoctorSpecializations: '/config/doctor-specialization/all'
+        getDoctorSpecializations: '/config/doctor-specialization/all',
+        getExaminationTypes: '/config/examination-type/all'
     }
 }
